00f157449d93257d8174c1daa3756cbcb866bc2e
qzh <826842941@qq.com>
2024-08-21 18:12:47 +0800

    1

---
+++ include/acg_renaming.h
@@ -0,0 +1,10 @@
+//
+// Created by dd on 2024/8/21.
+//
+
+#ifndef ACG_RENAMING_H
+#define ACG_RENAMING_H
+#pragma once
+
+
+#endif //ACG_RENAMING_H
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 //
 
 #include <stdio.h>
-#include "a.h"
 #include <curl/curl.h>
 int main() {
   curl_global_init(CURL_GLOBAL_DEFAULT);
